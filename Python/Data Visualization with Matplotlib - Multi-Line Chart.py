9e3554652d56c24cf76448cc8e01346829517695
Jonando Baskara <68272860+jonando93@users.noreply.github.com>
2020-09-22 02:07:16 +0700

    add plt.annotate()

--- Python/Data Visualization with Matplotlib - Multi-Line Chart.py
+++ Python/Data Visualization with Matplotlib - Multi-Line Chart.py
@@ -59,7 +59,6 @@ print(top_provinces)
 dataset['province_top'] = dataset['province'].apply(lambda x: x if (x in top_provinces['province'].to_list()) else 'other')
 
 # [3] Buat Multi-Line Chart - Data Breakdown by Top Provinces
-import matplotlib.pyplot as plt
 dataset.groupby(['order_month','province_top'])['gmv'].sum().unstack().plot(marker='.', cmap='plasma')
 plt.title('Monthly GMV Year 2019 - Breakdown by Province', loc='center',pad=30,fontsize=20, color='blue')
 plt.xlabel('Order Month', fontsize = 15)
