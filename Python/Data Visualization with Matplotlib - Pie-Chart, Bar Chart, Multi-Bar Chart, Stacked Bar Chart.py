8594f217bd66bf8d4f90d44ea1e47ae427e18680
Jonando Baskara <68272860+jonando93@users.noreply.github.com>
2020-09-22 10:51:55 +0700

    add Multi-Bar Chart & Stacked Bar Chart

---
+++ Python/Data Visualization with Matplotlib - Pie-Chart, Bar Chart, Multi-Bar Chart, Stacked Bar Chart.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 09:20:52 2020
+
+@author: jonando93
+"""
+
+#Import Library
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+
+# Baca Dataset
+dataset = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/retail_raw_reduced.csv')
+
+# Buat kolom baru yang bertipe datetime dalam format '%Y-%m'
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+
+# Buat Kolom GMV
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+# Buat variabel untuk 5 propinsi dengan GMV tertinggi
+top_provinces = (dataset.groupby('province')['gmv']
+                        .sum()
+                        .reset_index()
+                        .sort_values(by='gmv',ascending=False)
+                        .head(5))
+print(top_provinces)
+
+# Buat satu kolom lagi di dataset dengan nama province_top
+dataset['province_top'] = dataset['province'].apply(lambda x: x if (x in top_provinces['province'].to_list()) else 'other')
+
+# Buat Dataset baru untuk DKI Quarter 4
+dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
+print(dataset_dki_q4.head())
+
+# [1] Membuat Pie Chart untuk GMV setiap kota di DKI Jakarta
+gmv_per_city_dki_q4 = dataset_dki_q4.groupby('city')['gmv'].sum().reset_index()
+plt.figure(figsize=(6,6))
+plt.pie(gmv_per_city_dki_q4['gmv'], labels = gmv_per_city_dki_q4['city'],autopct='%1.2f%%') #autopct = 1.2 artinya terdapat 2 angka dibelakang koma (Jakarta Pusat = 24.98%)
+plt.title('GMV Contribution Per City - DKI Jakarta in Q4 2019',loc='center',pad=30,fontsize=15, color='blue')
+plt.show()
+
+# [2] Membuat Bar Chart untuk GMV setiap kota di DKI Jakarta
+dataset_dki_q4.groupby('city')['gmv'].sum().sort_values(ascending=False).plot(kind='bar', color='green')
+plt.title('GMV Per City - DKI Jakarta in Q4 2019', loc='center',pad=30, fontsize=15, color='blue')
+plt.xlabel('City', fontsize = 15)
+plt.ylabel('Total Amount (in Billions)',fontsize = 15)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(float))
+plt.xticks(rotation=0)
+plt.show()
+
+# [3] Membuat Multi-Bar Chart untuk GMV setiap kota di DKI Jakarta pada bulan Oct - Dec
+dataset_dki_q4.groupby(['city','order_month'])['gmv'].sum().unstack().plot(kind='bar')
+plt.title('GMV Per City, Breakdown by Month\nDKI Jakarta in Q4 2019',loc='center',pad=30, fontsize=15, color='blue')
+plt.xlabel('Province', fontsize = 12)
+plt.ylabel('Total Amount (in Billions)',fontsize = 12)
+plt.legend(bbox_to_anchor=(1, 1), shadow=True, title='Month')
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(float))
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+# [4] Membuat Stacked Bar Chart untuk GMV setiap kota di DKI Jakarta pada bulan Oct - Dec
+dataset_dki_q4.groupby(['order_month','city'])['gmv'].sum().sort_values(ascending=False).unstack().plot(kind='bar', stacked=True)
+plt.title('GMV Per Month, Breakdown by City\nDKI Jakarta in Q4 2019',loc='center',pad=30, fontsize=15, color='blue')
+plt.xlabel('Order Month', fontsize = 12)
+plt.ylabel('Total Amount (in Billions)',fontsize = 12)
+plt.legend(bbox_to_anchor=(1, 1), shadow=True, ncol=1,title='City')
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.xticks(rotation=0)
+plt.tight_layout()
+plt.show()
