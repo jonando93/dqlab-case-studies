66583a09351dc48f78d0807889e2e0b9f290dc2f
Jonando Baskara <68272860+jonando93@users.noreply.github.com>
2020-09-25 18:58:12 +0700

    added Visualization for Distribution of Customers per Region

---
+++ Python/Machine Learning - Data Exploration & Pre-processing.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 11:06:38 2020
+
+@author: jonando93
+"""
+
+#Import Library
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#Membaca Dataset
+dataset = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/pythonTutorial/online_raw.csv')
+
+#Checking Data Dimension for Data Exploration
+print('Shape dataset:', dataset.shape)
+print('\nLima data teratas:\n', dataset.head())
+print('\nInformasi dataset:')
+print(dataset.info())
+print('\nStatistik deskriptif:\n', dataset.describe())
+
+#Checking Feature Correlation
+dataset_corr = dataset.corr()
+print('Korelasi dataset:\n', dataset.corr())
+print('Distribusi Label (Revenue):\n', dataset['Revenue'].value_counts())
+
+#Checking Feature Correlation using .loc
+print('\nKorelasi BounceRates-ExitRates:', dataset_corr.loc['BounceRates', 'ExitRates'])
+print('\nKorelasi Revenue-PageValues:', dataset_corr.loc['Revenue', 'PageValues'])
+print('\nKorelasi TrafficType-Weekend:', dataset_corr.loc['TrafficType', 'Weekend'])
+
+# Checking the Distribution of customers on Revenue
+plt.rcParams['figure.figsize'] = (12,5)
+plt.subplot(1, 2, 1)
+sns.countplot(dataset['Revenue'], palette = 'pastel')
+plt.title('Buy or Not', fontsize = 20)
+plt.xlabel('Revenue or not', fontsize = 14)
+plt.ylabel('count', fontsize = 14)
+
+# Checking the Distribution of customers on Weekend
+plt.subplot(1, 2, 2)
+sns.countplot(dataset['Weekend'], palette = 'inferno')
+plt.title('Purchase on Weekends', fontsize = 20)
+plt.xlabel('Weekend or not', fontsize = 14)
+plt.ylabel('count', fontsize = 14)
+plt.show()
+
+# Visualizing the distribution of customers around the Region
+plt.hist(dataset['Region'], color = 'lightblue')
+plt.title('Distribution of Customers', fontsize = 20)
+plt.xlabel('Region Codes', fontsize = 14)
+plt.ylabel('Count Users', fontsize = 14)
+plt.show()
