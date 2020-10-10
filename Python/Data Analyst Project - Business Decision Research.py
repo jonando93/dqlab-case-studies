1ea9d60596c5633bf9f2912b8f11b6c7814def04
Jonando Baskara <68272860+jonando93@users.noreply.github.com>
2020-10-10 20:23:38 +0700

    add background

---
+++ Python/Data Analyst Project - Business Decision Research.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 19:28:37 2020
+
+@author: jonando93
+"""
+
+# DATA ANALYST PROJECT: BUSINESS DECISION RESEARCH
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#
+#Background:
+#DQLab sport center adalah toko yang menjual berbagai kebutuhan olahraga 
+#seperti Jaket, Baju, Tas, dan Sepatu. Toko ini mulai berjualan sejak tahun 
+#2013, sehingga sudah memiliki pelanggan tetap sejak lama, dan tetap berusaha 
+#untuk mendapatkan pelanggan baru sampai saat ini.
+#
+#Di awal tahun 2019,   manajer toko tersebut merekrut junior DA untuk membantu
+#memecahkan masalah yang ada di tokonya, yaitu menurunnya pelanggan yang 
+#membeli kembali ke tokonya.  Junior DA tersebut pun diberi kepercayaan 
+#mengolah data transaksi toko tersebut. Manajer toko mendefinisikan bahwa 
+#customer termasuk sudah bukan disebut pelanggan lagi (churn) ketika dia sudah
+#tidak bertransaksi ke tokonya lagi sampai dengan 6 bulan terakhir dari update 
+#data terakhir yang tersedia.  
+#
+#Manajer toko pun memberikan data transaksi dari tahun 2013 sampai dengan 2019 
+#dalam bentuk csv (comma separated value) dengan data_retail.csv dengan jumlah 
+#baris 100.000 baris data.
+#
+#
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# MARKET RESEARCH ,RECOMMENDATION AND VISUALIZATION TECHNIQUE FOR BUSINESS 
+# DECISION MAKING
